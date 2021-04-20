bb43cc40081f57562a839f06ae8415e2886ca3dd
Rakhee99 <80023884+Rakhee99@users.noreply.github.com>
2021-04-20 15:10:32 +0530

    Update loadWindowsScript.groovy

--- vars/loadWindowsScript.groovy
+++ vars/loadWindowsScript.groovy
@@ -1,5 +1,5 @@
 def call (Map config = [:]){
   def scriptcontents = libraryResource "com/scripts/windows/${config.fname}"
   writeFile file: "${config.fname}", text : scriptcontents
-  bat "chmod a+x ./${config.fname}"
+  bat "icacls RX ./${config.fname}"
 }
