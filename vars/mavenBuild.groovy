4b35490fb75693962351e6afd9d2d3f04e0b6bd8
Rakhee99 <80023884+Rakhee99@users.noreply.github.com>
2021-04-21 10:55:28 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,5 +1,9 @@
 def call()
 {
+  // Run Maven on a Unix agent.
+  // sh "mvn -Dmaven.test.failure.ignore=true clean package"
+
+  // To run Maven on a Windows agent, use
   bat "mvn -Dmaven.test.failure.ignore=true clean package"
 }
   
