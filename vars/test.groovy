c1e0aa908a7a09a7d62f146238537f471d9379d4
Rakhee99 <80023884+Rakhee99@users.noreply.github.com>
2021-04-21 10:56:25 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,7 @@
 def call()
 {
+      // If Maven was able to run the tests, even if some of the test
+      // failed, record the test results and archive the jar file.
       junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.war'
 }
