44b2f97119728d45903800a97d4fd127cab029b4
Rakhee99 <80023884+Rakhee99@users.noreply.github.com>
2021-04-22 15:56:11 +0530

    Update checkout.groovy

--- src/org/foo/checkout.groovy
+++ src/org/foo/checkout.groovy
@@ -1,6 +1,6 @@
 // src/org/foo/Zot.groovy
 package org.foo
-public class checkout{
+public class checkout implements Serializable {
 def checkOutFrom(repo) {
   git url: "https://github.com/Rakhee99/${repo}.git/"
 }
