2f1fbf8785044bbe3ea309a368fe86ad82d4ec17
Rakhee99 <80023884+Rakhee99@users.noreply.github.com>
2021-04-26 11:33:27 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -2,7 +2,7 @@ def call()
 {
   deploy adapters: [tomcat9(credentialsId: 'c086106e-65d8-4a23-b377-292a59e0b30f', 
                             path: '', 
-                            url: 'http://localhost:8081/')], 
+                            url: 'http://localhost:8083/')], 
                             contextPath: null, 
                             war: 'target/*.war'
 }
